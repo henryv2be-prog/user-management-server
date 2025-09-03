79816fe4b1f41ff547afb373c45c675d17ab5ca7
henryv2be-prog <henry.v2be@gmail.com>
2025-09-03 18:21:10 +0200

    Fix AccessGroup import/export mismatch that was causing 'function anonymous' error
    - Changed export from module.exports = { AccessGroup } to module.exports = AccessGroup
    - Updated all imports from const { AccessGroup } to const AccessGroup
    - This fixes the core issue that was present even in the 'working' commit
    - AccessGroup class should now be properly accessible in all routes

--- routes/accessGroups.js
+++ routes/accessGroups.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { AccessGroup } = require('../database/accessGroup');
+const AccessGroup = require('../database/accessGroup');
 const { 
   validateAccessGroup, 
   validateAccessGroupUpdate, 
